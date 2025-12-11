e542837949cd13151d1619300e35c27a22728ad9
Ravi kumar <119988156+gravikumar12@users.noreply.github.com>
2025-12-11 13:49:32 +0530

    Create app.module.ts

---
+++ app.module.ts
@@ -0,0 +1,9 @@
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [HttpClientModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
